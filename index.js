7ec48cbbc90e4b2c12e574dd3e8e026474880ee8
Joanna Elendu <joan_elendu@yahoo.com>
2024-05-02 09:09:39 -0500

    added node module and env file to gitignore

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require("express");
-const app = express();
+const express = require("express"); //imports express module
+const app = express(); // creates instance
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  // defines GET request made to the root
+  try {
+    res.send("Hello World");
+  } catch (err) {
+    // added error handling
+    console.error(err);
+    res.status(500).send("Error occured.");
+  }
 });
 
-const port = 3000;
+const port = 3000; // locally defined port server should listen to
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Listening on port ${port}`);
+});
+
+app.get("/test", (req, res) => {
+  //basic test to see if server works
+  res.send("Testing... Testing...");
 });
